4511a5985464e82a7399f152a7ca6698d488de3f
luan.silva <luanlucascorp@gmail.com>
2024-12-09 16:58:14 -0400

    senior

--- senior_cod_municipio.py
+++ senior_cod_municipio.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 # Caminho do arquivo principal e do arquivo de referência
 clientes_file = "clientes.csv"
-ibge_file = "cod_municipio.csv"
+codigos_file = "cod_municipio.csv"
 
 # Carregar o arquivo principal e o arquivo de referência
 clientes_df = pd.read_csv(clientes_file, sep=";", encoding="utf-8")
-ibge_df = pd.read_csv(ibge_file, sep=";", encoding="utf-8")
+codigos_df = pd.read_csv(codigos_file, sep=";", encoding="utf-8")
 
 # Garantir que os nomes das cidades estejam na mesma capitalização
 clientes_df["CLI_CIDADE"] = clientes_df["CLI_CIDADE"].str.strip().str.upper()
-ibge_df["cidade"] = ibge_df["cidade"].str.strip().str.upper()
+codigos_df["cidade"] = codigos_df["cidade"].str.strip().str.upper()
 
 # Mapear os códigos IBGE para a coluna CLI_CIDADE
-cidade_to_codigo = dict(zip(ibge_df["cidade"], ibge_df["codigo_ibge"]))
+cidade_to_codigo = dict(zip(codigos_df["cidade"], codigos_df["codigo_ibge"]))
 clientes_df["COD_MUNICIPIO"] = clientes_df["CLI_CIDADE"].map(cidade_to_codigo)
 
 # Remover valores NaN e converter para inteiro
